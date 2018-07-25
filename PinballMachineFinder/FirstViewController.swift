457bcac77da939af4448376575822bde4a835c29
JoshuaDanner <jdanner3@gmail.com>
2018-07-25 11:58:59 -0600

    Initial Commit

---
+++ PinballMachineFinder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PinballMachineFinder
+//
+//  Created by Joshua Danner on 7/25/18.
+//  Copyright © 2018 JoshuaDanner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PinballMachineFinder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PinballMachineFinder
+//
+//  Created by Joshua Danner on 7/25/18.
+//  Copyright © 2018 JoshuaDanner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
